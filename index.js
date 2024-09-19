cbe37523b01d5f9dc9793c52102c4022f0932012
Shiva Sunchu <shivasunchu06@gmail.com>
2024-09-19 12:59:09 +0530

    add few functions

--- index.js
+++ index.js
@@ -24,7 +24,7 @@
 
 // reference.appendChild(buttonElement);
 
-let buttonRef = document.getElementById("btn");
+// let buttonRef = document.getElementById("btn");
 
 // Event type, event target, action
 
@@ -41,18 +41,18 @@ let buttonRef = document.getElementById("btn");
 
 // click me -> clicked
 
-buttonRef.addEventListener("click", function abc(event) {
-  // best practices -> event / e
-  // event -> target property (the information of element upon which this event happened)
+// buttonRef.addEventListener("click", function abc(event) {
+//   // best practices -> event / e
+//   // event -> target property (the information of element upon which this event happened)
 
-  let eventTarget = event.target;
-  eventTarget.innerText = "Clicked";
+//   let eventTarget = event.target;
+//   eventTarget.innerText = "Clicked";
 
-  //   buttonRef.innerText = "Clicked";
+//   //   buttonRef.innerText = "Clicked";
 
-  console.log("Clicked");
-  console.log(event);
-});
+//   console.log("Clicked");
+//   console.log(event);
+// });
 
 // let eventObject = {
 //   keyOne: "ValueOne",
@@ -64,15 +64,79 @@ buttonRef.addEventListener("click", function abc(event) {
 //   },
 // };
 
-let inputRef = document.getElementById("input");
-let output = document.getElementById("output");
+// let inputRef = document.getElementById("input");
+// let output = document.getElementById("output");
 
-inputRef.addEventListener("input", (event) => {
+// inputRef.addEventListener("input", (event) => {
+//   console.log(event);
+//   //   let typedCharacter = event.data; // the input that is provided by the user by doing input event
+
+//   let typedCharacter = event.target.value;
+//   output.innerText = typedCharacter;
+// });
+
+// function clickHandler(event) {
+//   console.log(event);
+//   event.preventDefault();
+//   console.log(event.target.localName + " clicked");
+//   console.log("Inside click handler", event);
+// }
+
+// let aTagRef = document.querySelector("a");
+// aTagRef.addEventListener("click", clickHandler);
+
+// let formElement = document.querySelector("form");
+// formElement.addEventListener("submit", clickHandler);
+
+/**
+ * Problem statement
+ *
+ * 1. Input element <input type="text" placeholder="Type something" value="good morning"/>
+ * 2. Button element
+ * 3. After typing into the input element and clicking on the button I want to show the output in the console
+ *
+ *
+ */
+
+let container = document.getElementById("container");
+
+let inputElement = document.createElement("input");
+inputElement.setAttribute("type", "text");
+inputElement.setAttribute("placeholder", "Type something");
+inputElement.setAttribute("value", "Good morning");
+
+let buttonElement = document.createElement("button");
+buttonElement.innerText = "Submit";
+
+// append, appendChild
+
+container.append(inputElement, buttonElement);
+
+// let inputValue = ""; // assigning an empty value
+
+function clickHandler(event) {
   console.log(event);
-  //   let typedCharacter = event.data; // the input that is provided by the user by doing input event
+  let inputElement = event.target.previousSibling;
 
-  let typedCharacter = event.target.value;
-  output.innerText = typedCharacter;
-});
+  let containerElement = event.target.parentElement;
 
-// console.log(inputRef.placeholder);
+  // let window = event.view;
+
+  window.open("https://facebook.com");
+
+  containerElement.style.border = "1px solid black";
+  containerElement.style.padding = "2rem";
+
+  // console.log(inputElement);
+  // console.log(event);
+  console.log(inputElement.value);
+  console.log(containerElement);
+}
+
+// function inputHandler(event) {
+//   // event.target.value -> the value of the input element
+//   // inputValue = event.target.value;
+// }
+
+// inputElement.addEventListener("input", inputHandler);
+buttonElement.addEventListener("click", clickHandler);
